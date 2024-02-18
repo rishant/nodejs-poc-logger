4c449da2788eaab13ac8b50474af546eb6d68e75
Rishant G <rishantgupta007@gmail.com>
2024-02-18 23:03:38 +0530

    log4js: second example

--- nodejs-poc-logger-log4js/README.md
+++ nodejs-poc-logger-log4js/README.md
@@ -1,7 +1,10 @@
 # Create Node Prject:
+    mkdir nodejs-poc-logger-log4js
+    cd nodejs-poc-logger-log4js
     npm init 
 
 # Install log4js dependencies:
     npm install log4js
 
 ![screenshot_01.jpg](./screenshot_01.jpg)
+![screenshot_01.jpg](./screenshot_02.jpg)
---
+++ nodejs-poc-logger-log4js/index_01.js
@@ -0,0 +1,27 @@
+const log4js = require('log4js');
+
+// Logger configuration
+log4js.configure({
+    appenders: { 
+        fileAppender: { 
+            type: 'file', filename: './logs/index_01.log' 
+        } 
+    },
+    categories: { 
+        default: { 
+            appenders: ['fileAppender'], level: 'info' 
+        } 
+    }
+});
+
+// Create the logger
+const logger = log4js.getLogger();
+logger.level = 'info';
+
+// Logging messages with different levels
+logger.trace('This is a TRACE message.');
+logger.debug('This is a DEBUG message.');
+logger.info('This is an INFO message.');
+logger.warn('This is a WARN message.');
+logger.error('This is an ERROR message.');
+logger.fatal('This is a FATAL message.');
