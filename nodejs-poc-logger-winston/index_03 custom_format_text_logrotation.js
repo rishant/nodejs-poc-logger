01571082806c761cef25f216e552a281a4258062
Rishant G <rishantgupta007@gmail.com>
2024-02-21 01:39:39 +0530

    winston: logger with log file rotation & custom log format as in Either 'TEXT' Or 'JSON'

---
+++ nodejs-poc-logger-winston/index_03 custom_format_text_logrotation.js
@@ -0,0 +1,50 @@
+const rootPath = process.cwd();
+const path = require('path');
+const {createLogger, transports, format} = require('winston');
+const { timestamp, combine, printf, errors } = format;
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+let logFileDirPath = path.join(rootPath, 'logs');
+logFileDirPath = path.normalize(logFileDirPath);
+
+let logFileName = path.join(logFileDirPath, 'app.log');
+logFileName = path.normalize(logFileName);
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} ${level}: ${stack || message}`;
+});
+
+// Create a logger instance
+const logger = createLogger({
+  level: 'debug', // Set the default logging level
+  format: combine(
+    // format.colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }),
+    logFormat
+  ),
+  transports: [
+    new transports.Console(), // Log to console
+    // new transports.File({ filename: logFileName }), // Log to a file
+    new DailyRotateFile({
+      filename: `${path.join(logFileDirPath, 'app-%DATE%.log')}`,
+      datePattern: 'YYYY-MM-DD-HH',
+      // datePattern: 'YYYY',
+      zippedArchive: true,
+      maxSize: '1k',
+      maxFiles: '365d'
+    }),
+  ]
+});
+
+// Example usage
+logger.log('info', 'This is an informational message.');
+logger.log('error', 'This is an error message.');
+
+// Logging messages with different levels
+// logger.trace('This is a TRACE message.');
+logger.debug('This is a DEBUG message.');
+logger.info('This is an INFO message.');
+logger.warn('This is a WARN message.');
+logger.error('This is an ERROR message.');
+// logger.fatal('This is a FATAL message.');
